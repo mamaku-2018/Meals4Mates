c21aba86aaf2f6988e9b14db3e9268cb8b71b0f2
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-19 11:13:46 +1200

    added register reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux'
 
 import waiting from './waiting'
 import errorMessage from './errorMessage'
+import register from './register'
 
 export default combineReducers({
   errorMessage,
-  waiting
+  waiting,
+  register
 })
