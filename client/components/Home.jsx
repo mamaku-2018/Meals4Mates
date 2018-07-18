552dbc3f149afdb50de9b447c970677176da59f8
Indigo Phillips <indigogeorgiana@gmail.com>
2018-07-19 09:32:33 +1200

    Corrected component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
 
 import Banner from './Banner'
 import Locations from './Locations'
@@ -11,9 +10,9 @@ const Home = () => {
       <div className='columns'>
 
         <div className='column'>
-          <Route path='/' component={Banner} />
-          <Route path='/' component={Locations} />
-          <Route path='/' component={Advertising} />
+          <Banner />
+          <Locations />
+          <Advertising />
         </div>
       </div>
     </div>
