582ffd4fcf0ebb4e9cf127daaf758ffd46fa7077
Indigo Phillips <indigogeorgiana@gmail.com>
2018-07-19 10:01:51 +1200

    Completed header component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
+import Header from './Header'
 import Home from './Home'
 
 const App = () => {
   return (
     <Router>
       <div className='container'>
+        <Route path='/header' component={Header} />
         <Route exact path='/' component={Home} />
 
       </div>
