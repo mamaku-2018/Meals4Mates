403e41242b9f03a6f1dd190c0fced174e066e60b
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-19 11:51:39 +1200

    Created folder structure in tests/client and added saveAuthTokenMock to create tests for register

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
-
 import Login from './Auth/Login'
 import Header from './Header'
-import Home from './Home'
+// import Home from './Home'
 import Footer from './Footer'
+import Register from './Auth/Register'
 
 const App = () => {
   return (
     <Router>
       <div className='container'>
         <Route path='/' component={Header} />
-        <Route exact path='/' component={Home} />
+        {/* <Route exact path='/' component={Home} /> */}
         <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
 
         <Footer />
       </div>
