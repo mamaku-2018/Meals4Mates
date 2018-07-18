371526358d60d1e44ca92728448ffc2c182a2e17
anthony-xiao <juniorxiao01@gmail.com>
2018-07-19 09:18:25 +1200

    ran migration seed and all working

--- tests/server/db/seeds/stores.js
+++ tests/server/db/seeds/stores.js
@@ -4,9 +4,9 @@ exports.seed = (knex, Promise) => {
     .then(function () {
       // Inserts seed entries
       return knex('stores').insert([
-        {id: 1, name: 'store1', address: '1 test street', phone: 2111111, lat: 110, lng: 20, image_url: 'image1.jpg'},
-        {id: 2, name: 'store2', address: '2 test street', phone: 2111111, lat: 120, lng: 40, image_url: 'image2.jpg'},
-        {id: 3, name: 'store3', address: '3 test street', phone: 2111111, lat: 140, lng: 70, image_url: 'image3.jpg'}
+        {id: 1, name: 'store1', address: '1 test street', phone: 21111112, lat: 110, lng: 20, image_url: 'image1.jpg'},
+        {id: 2, name: 'store2', address: '2 test street', phone: 21111333, lat: 120, lng: 40, image_url: 'image2.jpg'},
+        {id: 3, name: 'store3', address: '3 test street', phone: 21112221, lat: 140, lng: 70, image_url: 'image3.jpg'}
       ])
     })
 }
