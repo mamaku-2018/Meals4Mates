fe2bc55178f2449ed5795a96b0324b60e8fb844e
sam-by-name <sam-sanders@hotmail.co.uk>
2018-07-18 20:10:03 +1200

    basic boilerplate is a go

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {HashRouter as Router} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <div className='container'>
+        <h1>Meals4Mates</h1>
+      </div>
+    </Router>
+  )
+}
+
+export default App
