d0cb8913d4da56bd0a1c657213a3f97aafd99de2
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-19 11:21:03 +1200

    Added userDetails reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 
 import waiting from './waiting'
 import errorMessage from './errorMessage'
-import register from './register'
+import register from './auth/register'
+import userDetails from './auth/userDetails'
 
 export default combineReducers({
   errorMessage,
   waiting,
-  register
+  register,
+  userDetails
 })
