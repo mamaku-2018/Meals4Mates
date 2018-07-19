aaa37b49ff5fcfeeff0d9983c87f5b7e5d1ce80f
anthony-xiao <juniorxiao01@gmail.com>
2018-07-19 16:40:36 +1200

    changed db stores functions

--- server/db/functions/store.js
+++ server/db/functions/store.js
@@ -10,37 +10,18 @@ module.exports = {
 
 function getStoreDetails (id, db = knex) {
   return db('stores as s')
-    .join('users as u', 's.id', 'u.store_id')
     .where('s.id', id)
-    .select('u.email as email', 'u.hash as hash', 'u.name as owner',
-      's.name as name', 's.address as address', 's.phone as phone')
+    .select()
 }
 
-// new store object strcture = {user.name, user.hash, user.email, stores.name, stores.address, stores.phone, stores.lat, stores.lng}
 function addNewStore (newStore, db = knex) {
   return db('stores as s')
-    .join('users as u', 's.id', 'u.store_id')
-    .insert({
-      name: newStore.name,
-      address: newStore.address,
-      phone: newStore.phone,
-      lat: newStore.lat,
-      lng: newStore.lng
-    }).into('stores')
-    .insert({
-      name: newStore.owner,
-      email: newStore.email,
-      hash: newStore.hash
-    }).into('users')
+    .insert(newStore)
 }
 
 function editStoreDetails (store, db = knex) {
   return db('stores as s')
-    .join('users as u', 's.id', 'u.store_id')
     .where('s.id', store.id)
-    .select('u.email as email', 'u.hash as hash', 'u.name as owner',
-      's.name as name', 's.address as address', 's.phone as phone',
-      's.lat as lat', 's.lng as lng')
     .update(
       'name', store.name,
       'address', store.address,
