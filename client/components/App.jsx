5899836e1be1ce858f78fb4c8be09894e5337576
sam-by-name <sam-sanders@hotmail.co.uk>
2018-07-19 16:12:12 +1200

    Footer given correct path

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import Login from './Auth/Login'
+import Register from './Auth/Register'
 import Header from './Header'
 // import Home from './Home'
 import Footer from './Footer'
-import Register from './Auth/Register'
 
 const App = () => {
   return (
@@ -14,8 +14,7 @@ const App = () => {
         {/* <Route exact path='/' component={Home} /> */}
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
-
-        <Footer />
+        <Route path='/' component={Footer} />
       </div>
     </Router>
   )
