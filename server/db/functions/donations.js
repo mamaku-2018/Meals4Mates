1daf0fef95f70d1c3cb9567ec60b5978a3fdac8d
Sam <36747880+sam-by-name@users.noreply.github.com>
2018-07-20 09:34:40 +1200

    Update donations.js

--- server/db/functions/donations.js
+++ server/db/functions/donations.js
@@ -1,4 +1,4 @@
-var environment = process.env.NODE_ENV || 'development'
+let environment = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[environment]
 const knex = require('knex')(config)
 
