77aced98886fbae4fe7ff37da6f0b9f8e89ae159
anthony-xiao <juniorxiao01@gmail.com>
2018-07-20 00:45:31 +1200

    trying out using promise all

--- server/db/functions/balance.js
+++ server/db/functions/balance.js
@@ -3,13 +3,30 @@ const config = require('../knexfile')[environment]
 const knex = require('knex')(config)
 
 module.exports = {
-  getCurrentBalance
+  getCurrentBalance,
+  getStoreTotalDonation,
+  getStoreTotalRedemption
 }
 
+//function returns a array of an object contatining total donation and redemption of the store. these two figures can be used to generate datta for store balance circle as well as bar graph
 function getCurrentBalance (id, db = knex) {
   return db('balance')
     .where('store_id', id)
     .sum('donation as donation')
     .sum('redemption as redemption')
-    .then(figures => figures[0].donation - figures[0].redemption)
 }
+
+function getStoreTotalDonation (id, db = knex) {
+  return db('balance')
+    .where('store_id', id)
+    .sum('donation as donation')
+    .then(donation => donation[0])
+}
+
+function getStoreTotalRedemption (id, db = knex) {
+  return db('balance')
+    .where('store_id', id)
+    .sum('redemption as redemption')
+    .then(redemption => redemption[0])
+}
+
