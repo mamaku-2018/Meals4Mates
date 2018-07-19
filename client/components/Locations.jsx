d0534008a16c855534b0e9c685915fc453b004a2
bronburgundy <bron.cotter@icloud.com>
2018-07-19 14:16:00 +1200

    Added Locations and Map components

---
+++ client/components/Locations.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Map from './Map'
+
+const Locations = () => {
+  return (
+    <div className='locations'>
+      <div id='map'>
+        <Map />
+      </div>
+    </div>
+  )
+}
+
+export default Locations
