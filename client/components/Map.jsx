4df377c9aeab31e9b4b830e343ecbb60911c8e5e
bronburgundy <bron.cotter@icloud.com>
2018-07-19 16:13:04 +1200

    Got map to work! Whooop whooop

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -3,19 +3,13 @@ import L from 'leaflet'
 
 class Map extends React.Component {
   componentDidMount () {
-    this.map = L.map('map', {
-      center: [-36.8485, 174.7633],
-      zoom: 13,
-      layers: [
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox.streets',
-          accessToken: 'pk.eyJ1IjoiYnJvbmJ1cmd1bmR5IiwiYSI6ImNqanJ3N3hlYzhvb2sza2xmdGZocmwzMHgifQ.W5lq17kl4kLbi4qmQ1DNrg'
-        })
-        // .addTo(this.map)
-      ]
-    })
+    let mymap = L.map('map').setView([-36.8485, 174.7633], 13)
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiYnJvbmJ1cmd1bmR5IiwiYSI6ImNqanJ3N3hlYzhvb2sza2xmdGZocmwzMHgifQ.W5lq17kl4kLbi4qmQ1DNrg'
+    }).addTo(mymap)
   }
   render () {
     return <div id='map'></div>
