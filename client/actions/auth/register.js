0ffe2af9521267f5d18b7a219a73f42b9fb446f5
anthony-xiao <juniorxiao01@gmail.com>
2018-07-23 11:10:15 +1200

    made google api key from process env

--- client/actions/auth/register.js
+++ client/actions/auth/register.js
@@ -2,13 +2,8 @@ import {showError, clearError, showSuccess} from '../'
 import {saveAuthToken} from '../../lib/auth'
 import request from '../../lib/apiClient'
 import Geocode from 'react-geocode'
-import aws from 'aws-sdk'
 
-let s3 = new aws.S3({
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY
-})
-
-Geocode.setApiKey(`${s3.GOOGLE_API_KEY}`)
+Geocode.setApiKey(process.env.GOOGLE_API_KEY)
 Geocode.enableDebug()
 
 export const REQUEST_USER_REGISTRATION = 'REQUEST_USER_REGISTRATION'
