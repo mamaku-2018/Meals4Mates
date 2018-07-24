a5422775e623a3d96be8c36bd43965dfbf29e9e9
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-07-24 15:32:05 +1200

    reg btn works after register

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {clearError} from '../actions/'
 
 import Banner from './Banner'
 import Locations from './Locations'
 import Advertising from './Advertising'
 
-const Home = () => {
-  return (
-    <div className='main-container'>
-      <Banner />
-      <Locations />
-      <Advertising />
-    </div>
+class Home extends React.Component {
+  componentDidMount () {
+    this.props.clearError()
+  }
+  render () {
+    return (
+      <div className='main-container'>
+        <Banner />
+        <Locations />
+        <Advertising />
+      </div>
 
-  )
+    )
+  }
 }
 
-export default Home
+function mapDispatchToProps (dispatch) {
+  return {
+    clearError: () => {
+      dispatch(clearError())
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Home)
