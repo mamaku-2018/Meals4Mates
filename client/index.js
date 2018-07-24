4025db1a8fb97ae2f4fecee23826243544645ce2
anthony-xiao <juniorxiao01@gmail.com>
2018-07-24 19:51:51 +1200

    able to keep state on refresh

--- client/index.js
+++ client/index.js
@@ -4,18 +4,33 @@ import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import {PersistGate} from 'redux-persist/integration/react'
+
 import reducers from './reducers'
 import App from './components/App'
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers, composeEnhancers(
+const store = createStore(persistedReducer, composeEnhancers(
   applyMiddleware(thunkMiddleware)
 ))
+
+let persistor = persistStore(store)
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </ PersistGate>
     </Provider>,
     document.getElementById('app')
   )
