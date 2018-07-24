b223b71cdca656be8e3f438d6fd8c95f52bd7a86
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-25 10:48:58 +1200

    refactored monthly stats into actions so it can be used by two components

--- client/actions/storeStats.js
+++ client/actions/storeStats.js
@@ -23,10 +23,56 @@ export function getStoreStats (id) {
     return request
       .get(`/api/v1/store/${id}/donationRedemption`)
       .then(res => {
-        dispatch(receiveStoreStats(res.body))
+        return getTransactions(res.body)
+      })
+      .then(stats => {
+        dispatch(receiveStoreStats(stats))
       })
       .catch(() => {
         dispatch(showError('An unexpected error has occurred'))
       })
   }
+}
+
+let dates = []
+let donations = []
+let redemptions = []
+
+function isTransactionDateSummedUp (date) {
+  return dates.indexOf(date.substring(0, 7)) !== -1
+}
+
+function sumUpTransactionDate (date, stats) {
+  let donationSum = 0
+  let redemptionSum = 0
+
+  stats.forEach(t => {
+    if (t.date.substring(0, 7) === date.substring(0, 7)){
+      donationSum += Number(t.donation)
+      redemptionSum += Number(t.redemption)
+    }
+  })
+
+  dates.push(date.substring(0, 7))
+  donations.push(donationSum)
+  redemptions.push(redemptionSum)
+}
+
+export function getTransactions (stats) {
+  stats.forEach(t => {
+    if(!isTransactionDateSummedUp(t.date)) {
+      sumUpTransactionDate(t.date, stats)
+    }
+  })
+
+  let obj = {}
+  dates.forEach((d, i) => {
+    obj[i] = {
+      date: d,
+      donation: donations[i],
+      redemption: redemptions[i]
+    }
+  })
+  const transactions = Object.values(obj)
+  return transactions
 }
