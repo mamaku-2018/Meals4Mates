219845c23dffb7bef43cea03e1f307998804e464
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-24 22:14:06 +1200

    fixed one test

--- tests/client/components/Auth/Register.test.js
+++ tests/client/components/Auth/Register.test.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import {Register} from '../../../../client/components/Auth/Register'
-import {configure, shallow} from 'enzyme'
+import {configure, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import {Provider} from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+import configureStore from 'redux-mock-store'
+
+//create initial state
+const initialState = {
+  getAllStoreStats: [
+    {id: 1, store: 'store 1'},
+    {id: 2, store: 'store 2'}
+  ]
+}
+const mockStore = configureStore([thunkMiddleware])
+let store
 
 configure({adapter: new Adapter()})
 
 test('<Register /> component has register 8 input fields', () => {
+  store = mockStore(initialState)
   const expected = 9
   const testFn = jest.fn()
-  const wrapper = shallow(<Register dispatch={testFn} />)
+  const wrapper = mount(<Provider store={store}><Register /></Provider>)
   const actual = wrapper.find('input').length
   expect(actual).toEqual(expected)
 })
--- tests/client/components/Donations.test.js
+++ tests/client/components/Donations.test.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import {Donations} from '../../../client/components/Donations'
-import {configure, shallow} from 'enzyme'
+import {configure, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import {Provider} from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+import configureStore from 'redux-mock-store'
+
+//create initial state
+const initialState = {
+  getAllStoreStats: [
+    {id: 1, store: 'store 1'},
+    {id: 2, store: 'store 2'}
+  ]
+}
+const mockStore = configureStore([thunkMiddleware])
+let store
 
 configure({adapter: new Adapter()})
 
 test('<Donations /> component has six buttons', () => {
+  store = mockStore(initialState)
   const expected = 6
   const testSubmitDonation = jest.fn()
-  const wrapper = shallow(<Donations dispatch={testSubmitDonation} />)
+  const wrapper = mount(<Provider store={store}><Donations dispatch={testSubmitDonation} /></Provider>)
   const actual = wrapper.find('button').length
   expect(actual).toEqual(expected)
 })
