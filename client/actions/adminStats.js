1f6e132d7054f3b41d17acefaba0f2984b138fb4
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-24 18:41:43 +1200

    adding admin chart actions

---
+++ client/actions/adminStats.js
@@ -0,0 +1,32 @@
+import request from 'superagent'
+import {showError} from './'
+
+export const REQUEST_ADMIN_STATS = 'REQUEST_ADMIN_STATS'
+export const RECEIVE_ADMIN_STATS = 'RECEIVE_ADMIN_STATS'
+
+const requestAdminStats = () => {
+  return {
+    type: REQUEST_ADMIN_STATS
+  }
+}
+
+const receiveAdminStats = (stats) => {
+  return {
+    type: RECEIVE_ADMIN_STATS,
+    stats
+  }
+}
+
+export function getAdminStats() {
+  return dispatch => {
+    dispatch(requestAdminStats())
+    return request
+      .get('/api/v1/store/donationRedemption')
+      .then(res => {
+        dispatch(receiveAdminStats(res.body))
+      })
+      .catch(() => {
+        dispatch(showError('An unexpected error has occurred'))
+      })
+  }
+}
