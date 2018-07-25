746f19d7f1ff9e703b22b36776e6e069043262d8
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-25 14:47:06 +1200

    StoreInfo.test setup

--- tests/client/components/StoreInfo.test.js
+++ tests/client/components/StoreInfo.test.js
@@ -1,4 +1,15 @@
 import React from 'react'
 import {configure, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import StoreInfo from '../../'
+import StoreInfo from '../../../client/components/Store/StoreInfo'
+import {Provider} from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+import configureStore from 'redux-mock-store'
+
+const initialState = {
+  userDetails: [
+    name: 'Test user',
+    address: '123 Test St',
+    suburb:
+  ]
+}
