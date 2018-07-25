99d03ae4ec9d8f8d6e1ad43195f7191cc00b105a
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-07-25 17:28:47 +1200

    updated test to match seeds

--- tests/server/db/balance.test.js
+++ tests/server/db/balance.test.js
@@ -15,6 +15,6 @@ afterEach(() => {
 test('getCurrentBalance gets the total balance remaining of a store', () => {
   return db.getTotalDonations(testDb)
     .then(total => {
-      expect(total[0].donation).toBe(2369)
+      expect(total[0].donation).toBe(2214)
     })
 })
